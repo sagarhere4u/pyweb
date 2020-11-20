c4b2d08475b6062e4d51c58947625a21c306fc60
sagarhere4u <55478536+sagarhere4u@users.noreply.github.com>
2020-11-20 19:41:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "kubernetes_deployment" "pyweb-deployment" {
 
       spec {
         container {
-          image = "smehta26/pyweb:2.0"
+          image = "smehta26/pyweb:1.0"
           name  = "pyweb"
           port {
             container_port = 5000
